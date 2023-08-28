fbd7442a0459b28a91ad122d68e85c917f895c65
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-08-28 17:13:39 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,88 @@
+/*
+let nome = prompt("Qual seu nome?");
+let cor = prompt("Qual sua cor favorita?");
+
+console.log("A cor favorita de " + nome + " é" + cor);
+console.log(`A cor favorita de ${nome} é ${cor}`);
+
+
+console.log(nome.length);
+//retornar o número de caracteres em uma string;
+
+console.log(nome.toLowerCase());
+//deixar as letras em minusculas;
+
+console.log(nome.toUpperCase());
+//letras em maiusculas;
+
+console.log(nome.trim());
+//retira os espaços ao redor da string;
+
+console.log(nome.includes("cenoura"));
+//metodo para buscar determinado nome em uma string;
+
+console.log(nome.replaceAll("cenoura","batata"));
+//metodo para trocar uma palavra existente em uma string em outra;
+
+
+//exercicio 2
+
+let frase = prompt("Escreva uma frase");
+
+console.log(frase.toUpperCase());
+console.log(frase.replaceAll("o","i"));
+console.log(frase.length);
+
+
+//Arrays
+let listaDeExercicios = ["elevacao lateral","supino","supino"];
+
+console.log(listaDeExercicios[0]);
+console.log(listaDeExercicios[1]);
+console.log(listaDeExercicios[2]);
+console.log(listaDeExercicios);
+
+//exercicio 3
+let racasDeCachorro = ["Pug","Pastor Alemao","Poodle","Labrador","Buldogue"];
+let numero = Number (prompt("Digite um número de 0 a 4"));
+
+console.log(racasDeCachorro[numero]);
+
+--------//------------
+
+const pokemon = ["bulbasauro","squirtle","charmander"];
+
+console.log(pokemon.length);
+console.log(pokemon[0].length);
+
+const seriesBoas = ["Breaking Bad","Brooklyn nine-nine",]
+
+console.log(seriesBoas.includes("Breaking Bad"));
+console.log(seriesBoas.includes("Game of Thrones"));
+
+---------//-----------
+
+let instrumentos = ["violao","guitarra", "bateria"];
+
+console.log(instrumentos)
+
+instrumentos.push("teclado");
+
+console.log(instrumentos);
+
+//instrumentos.pop();
+
+console.log(instrumentos);
+instrumentos.splice(2, 2);
+console.log(instrumentos);
+
+//exercicio 4
+let numero = [1, 2, 3, 4, 5, 6];
+
+console.log(numero.length);
+console.log(numero);
+numero.push(7);
+numero.splice(3, 2);
+console.log(numero);
+console.log(numero.length)
+*/
